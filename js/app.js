72d927960e40a4bd1b890c7ada74d35e7be3af97
Asa Toburen <atopettit@gmail.com>
2015-02-12 08:04:44 -0700

    fixed if statements to check the value of hands and corrected bracket spacing

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
 
-  var Suit = function(name, abbr, char) {
-    this.name = name;
-    this.abbr = abbr;
-    this.char = char;
-  };
+var Suit = function(name, abbr, char) {
+  this.name = name;
+  this.abbr = abbr;
+  this.char = char;
+};
  
-  var suits = {
-    0: new Suit('Clubs', 'C', '&clubs;'),
-    1: new Suit('Diamonds', 'D', '&diams;'),
-    2: new Suit('Hearts', 'H', '&hearts;'),
-    3: new Suit('Spades', 'S', '&spades;')
-  };
+var suits = {
+  0: new Suit('Clubs', 'C', '&clubs;'),
+  1: new Suit('Diamonds', 'D', '&diams;'),
+  2: new Suit('Hearts', 'H', '&hearts;'),
+  3: new Suit('Spades', 'S', '&spades;')
+};
 
 // Class for Card
 
 var Card = function(suit, value) {
-    this.suit = suit;
-    this.value = value;
-    this.visible = false;
+  this.suit = suit;
+  this.value = value;
+  this.visible = false;
 };
 
 Card.prototype.getValue = function() {
-    return (this.value > 10) ? 10 : this.value;
+  return (this.value > 10) ? 10 : this.value;
 };
 
 Card.prototype.getName = function() {
@@ -29,32 +29,30 @@ Card.prototype.getName = function() {
       return this.value;
     } else {
       switch (this.value) {
-      case 1:
-        return 'A';
-      case 11:
-        return 'J';
-      case 12:
-        return 'Q';
-      case 13:
-        return 'K';
+        case 1:
+          return 'A';
+        case 11:
+          return 'J';
+        case 12:
+          return 'Q';
+        case 13:
+          return 'K';
       }
     }
-  };
+};
 
 Card.prototype.getSuit = function() {
-     return suits[this.suit];
-  };
+  return suits[this.suit];
+};
 
 Card.prototype.display = function() {
-    if (!this.visible) {
-      return $('<div class="card ' + suits[this.suit].name + '">' +
-                 '<span class="rank">' + this.getName() + '</span>' +
-                 '<span class="suit">' + suits[this.suit].char + '</span>' +
-               '</div>').data('card', this);
-        }
-  };
+    return $('<div class="card ' + suits[this.suit].name + '">' +
+              '<span class="rank">' + this.getName() + '</span>' +
+              '<span class="suit">' + suits[this.suit].char + '</span>' +
+              '</div>').data('card', this);
+};
  
- Card.prototype.toggleVisibility = function() {
+Card.prototype.toggleVisibility = function() {
     this.visible = !this.visible;
     return this.display();
 };
@@ -68,7 +66,6 @@ var Deck = function() {
 };
 
 // Method to build deck
-
 
 Deck.prototype.build = function() {
     this.array = [];
@@ -94,155 +91,150 @@ Deck.prototype.shuffle = function() {
         this.array[i] = t;
         this.shuffled = 1;
     }
-     return this.array;
+    return this.array;
 };
-//https://github.com/TNowalk/blackjack-jquery/blob/master/assets/js/app.js#L106 
-//http://tnowalk.github.io/blackjack-jquery/
-//https://github.com/TNowalk/blackjack-jquery/blob/master/assets/js/app.js#L106
 
 
 Deck.prototype.reset = function() {
-    this.array = this.resetDeck;
-    this.shuffled = 0;
+  this.array = this.resetDeck;
+  this.shuffled = 0;
 };
 
 Deck.prototype.deal = function() {
-     return this.array.shift();
+  return this.array.shift();
 };
 
-Deck.prototype.display = function() {
-
-  };
-
 var Hand = function(container) {
-    this.cards = [];
-    this.status = "";
-    this.container = container;
+  this.cards = [];
+  this.status = "";
+  this.container = container;
 };
 
 Hand.prototype.value = function() {
-    var value = 0;
-    var aces = 0;
-    for (var i = 0; i < this.cards.length; i++) {
-        if (this.cards[i].getValue() === 1) {
+  var value = 0;
+  var aces = 0;
+  for (var i = 0; i < this.cards.length; i++) {
+    if (this.cards[i].getValue() === 1) {
         aces++;
-      }
-         value += this.cards[i].getValue();
-  }
+    }
+      value += this.cards[i].getValue();
+    }
     if(aces && value <= 12) {
         value += 10;
     }
     return value;
-  };
+};
 
 Hand.prototype.recieveCard = function(card) {
     this.cards.push(card);
 };
 
 Hand.prototype.check = function() {
- var userArea = document.getElementById(this.container); 
-    if(this.value() > 21){
-        this.status = 'bust';
-        userArea.style.backgroundColor = "#d03111";
-        return this.status;
+  var userArea = document.getElementById(this.container); 
+    if(this.value() > 21) {
+      $("#dealerHand div:last").attr('id', 'card-show');
+      this.status = 'bust';
+      userArea.style.backgroundColor = "#d03111";
     } else if (this.value() === 21 ) {
-         this.status = 'Twenty One!!'; 
-         userArea.style.backgroundColor = "#3cbf77";
-         return this.status;
+      $("#dealerHand div:last").attr('id', 'card-show');
+      this.status = 'Twenty One!!'; 
+      userArea.style.backgroundColor = "#3cbf77";
     } else {
-        this.status = "none";
-        return this.status;
-        }
-    };
+      this.status = "none";
+    }
+};
 
 Hand.prototype.display = function(destination) {
- var inText = document.getElementById(destination);
-  for(var x = 0; x<this.cards.length; x++){
-        $( this.cards[x].display()).appendTo( inText );
+  for(var x = 0; x<this.cards.length; x++) {
+      $( this.cards[x].display()).appendTo('#' + destination );
   }
-
-   if(destination === 'dealerHand') {
-    inText.childNodes[4].id = 'card-hidden';
+  if(destination === 'dealerHand') {
+    $("#dealerHand div:last").attr('id', 'card-hidden');
   }
 };
 
 var deckOne;
-var deckHTML = document.getElementById('deck');
 var userHand = new Hand('userHand');
 var dealerHand = new Hand('dealerHand');
 
 
 $('#build').on('click', function() {
-     deckOne = new Deck();
-     deckOne.build();
-     deckHTML.style.backgroundColor = "#d03111";
-     console.log(deckOne);
+  deckOne = new Deck();
+  deckOne.build();
+  $('#deck').css('background-color', '#d03111');
 });
 
 $('#shuffle').on('click', function() {
-   if(deckOne !== undefined) {
+  if(deckOne !== undefined) {
     deckOne.shuffle();
-    console.log(deckOne);
-    deckHTML.style.backgroundColor = "#3cbf77";
-
-    }
+    $('#deck').css('background-color', '#3cbf77');
+  }
 });
 
 
 $('#deal').on('click', function() {
-    if(deckOne !== undefined && deckOne.shuffled !== 0){
+
+  if(deckOne !== undefined && deckOne.shuffled !== 0) {
      
-     for (var i = 1; i <= 2; i++) { 
-         userHand.recieveCard(deckOne.deal());
-         dealerHand.recieveCard(deckOne.deal());
+    for (var i = 1; i <= 2; i++) { 
+      userHand.recieveCard(deckOne.deal());
+      dealerHand.recieveCard(deckOne.deal());
     }
-        userHand.display('userHand');
-        dealerHand.display('dealerHand');
+
+    userHand.display('userHand');   
+    dealerHand.display('dealerHand');
         
        console.log("User Value: " + userHand.value());
        console.log("Dealer Value: " + dealerHand.value());    
-    }
+  }
     userHand.check(); 
+    dealerHand.check();
 });
 
-
-
 $('#hit').on('click', function(){
-    var inText = document.getElementById('userHand');
-    if(userHand.value() < 21 ){ 
-        userHand.recieveCard(deckOne.deal());
-    var hitCard = userHand.cards[userHand.cards.length - 1].display();
-        hitCard.appendTo(inText);
-        console.log("User Value: " + userHand.value());
-        console.log("Dealer Value: " + dealerHand.value());
-        console.log(userHand.check());
-        userHand.check();
-    }
+    
+  if(userHand.value() < 21 ){ 
+
+    var hitCard = deckOne.deal(); 
+    userHand.recieveCard(hitCard);
+    $('#userHand').append(hitCard.display());
+    console.log("User Value: " + userHand.value());
+    console.log("Dealer Value: " + dealerHand.value());
+    console.log(userHand.check());
+    userHand.check();
+  }
 });
 
 // Stand-button event listener 
 
 $('#stand').on('click', function() {
-    var inText = document.getElementById('dealerHand');
-        inText.childNodes[4].id = 'card-show';
-    if(userHand.value() <= 21 && dealerHand.value() <= userHand.value()){
-
-        if(dealerHand.value() <= 17) {
-             dealerHand.recieveCard(deckOne.deal());
-            var hitCard = dealerHand.cards[dealerHand.cards.length - 1].display();
-                 hitCard.appendTo(inText);
-        } else if (dealerHand.value() > userHand.value()) {
-            inText.style.backgroundColor = "#3cbf77";
-        }
+  
+  $("#dealerHand div:last").attr('id', 'card-show');
+  
+  userHand.check();
+  dealerHand.check();
+  
+    if( dealerHand.value() > userHand.value()) {
+      $('#dealerHand').css('background-color', "#3cbf77");
+    } else if(userHand.value() > dealerHand.value() && dealerHand.value >= 17) {
+      $('#userHand').css('background-color', "#3cbf77"); 
+    } else if(dealerHand.value() > userHand.value()) {
+      $('#dealerHand').css('background-color', "#3cbf77"); 
+    } else if (userHand.value() < 21 && dealerHand.value() < userHand.value() ) {
+     
+      while (dealerHand.value() < 17) {
+        var hitCard = deckOne.deal();  
+        dealerHand.recieveCard(hitCard);
+        hitCard.display();
+        dealerHand.check(); 
+      } 
+    } else {
+         $('#dealerHand').css('background-color', "#4f7cac"); 
+         $('#userHand').css('background-color', "#4f7cac"); 
     }
-    console.log("User Value: " + userHand.value());
-    console.log("Dealer Value: " + dealerHand.value());
-    console.log(userHand.value());
-    console.log(dealerHand.value());
-    userHand.check();
-    dealerHand.check();
+        console.log("User Value: " + userHand.value());
+        console.log("Dealer Value: " + dealerHand.value());
 });
-
 
 
 
